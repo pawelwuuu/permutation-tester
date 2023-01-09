ca5f84d678ddfd99a8821ed3fdbcbeb9260cdda2
Pawel Wojcik <wojcikp376@gmail.com>
2023-01-09 16:06:48 -0500

    added testing for type a of tests

--- tester.sh
+++ tester.sh
@@ -48,23 +48,129 @@ if [[ ${fileContent: -1} != '$' ]]; then
     exit 5
 fi
 
-#loading parameters to arrays: setLength and permutationsAmount.
-characters=($(cat $filename))
-for (( i=0; i<${#characters[@]}; i++ ))
-do
-	if [[ $i -eq 0 ]]
-	then
-		numOfLines=`wc -l $filename | grep -Po "\\d+"`
-		let numOfLines++
-		let "requirdeNumLines = characters[0] * 2 + 2"
+#checking if first line is number and if there is enough lines of data in file
+fistLine=`head -n 1 $filename`
 
-		if ! [[ $numOfLines -eq $requirdeNumLines ]]
+if ! echo "$fistLine" | grep -Eq '^[0-9]+$'; then
+    echo "First line must be a number of tests aplied."
+	exit 6
+fi
+
+numOfLines=`wc -l $filename | grep -Po "\\d+"`
+let numOfLines++
+let "requirdeNumLines = fistLine * 2 + 2"
+
+if ! [[ $numOfLines -eq $requirdeNumLines ]]
+then
+	echo "Incorrect number of lines in config file"
+	exit 7
+fi
+
+#loading parameters and validating them
+IFS=$'\n' read -d '' -r -a lines < $filename
+
+#creating logs file if does not exist
+if ! [[ -e ./logs ]]
+then
+	mkdir logs
+	if (( $? != 0 ))
+	then
+		echo "Unable to create logs folder."
+		exit 10
+	fi
+fi
+
+#creating folder id and folder inside logs folder
+folderIdentifier=$(date +"%Y%m%d%H%M%S")
+folderRelPath="./logs/$folderIdentifier"
+if [[ -e $folderRelPath ]]
+then
+	echo "Unable to create test folder."
+	exit 11
+fi
+mkdir $folderRelPath
+
+#setting timeformat to seconds
+TIMEFORMAT=%R
+
+#creating temp file which contain relative paths to times
+if [[ -e tmpFile.txt ]]
+then
+	rm tmpFile.txt
+	touch tmpFile.txt
+fi
+
+generatorCfgRel="./permutations/config.cfg"
+for (( i=0; i<${#lines[@]}; i++ ));
+do
+	if (( $i % 2 != 0 ))
+	then
+		if echo "${lines[$i]}" | grep -Eq '^A$'
 		then
-			echo "Incorrect number of lines in config file"
-			exit 6
+			#test of type A
+			let "parTestA = i + 1"
+    		if echo "${lines[$parTestA]}" | grep -Eq '^[0-9]+([ ]+[0-9]+)*$'
+			then
+				#creating config file for generator
+				if [[ -e $generatorCfgRel ]]
+				then
+					rm $generatorCfgRel
+				fi
+				touch $generatorCfgRel
+
+				#creating txt file with times
+				fileRelPath="$folderRelPath/A$i"
+				touch $fileRelPath
+				if (( $? != 0 ))
+				then
+					echo "Unable to create times file."
+					exit 12
+				fi
+
+				#appending params to config file and launching generator
+				params=(${lines[$parTestA]})
+				for param in "${params[@]}"
+				do
+				   echo "$param 0" >> $generatorCfgRel
+				done
+
+				#appending data to file test a
+				echo "A" >> $fileRelPath
+
+				cd ./permutations
+				/usr/bin/time -ao ../$fileRelPath -f "%e" ./pdfGenerator.sh ./config.cfg
+				cd ..
+
+				for param in "${params[@]}"
+				do
+				   echo "$param" >> $fileRelPath
+				done
+
+				echo $fileRelPath >> tmpFile.txt
+
+			else
+				let parTestA++
+    			echo "Error in line $parTestA of config file, such syntax of test does not exist."
+				exit 8
+			fi
+		elif echo "${lines[$i]}" | grep -Eq '^B [0-9]+$'
+		then
+    		#test of type B
+			let "parTestB = i + 1"
+    		if echo "${lines[$parTestB]}" | grep -Eq '^[0-9]+([ ]+[0-9]+)*$'
+			then
+    			echo "Linia zawiera liczby oddzielone spacją."
+			else
+				let parTestB++
+    			echo "Error in line $parTestB of config file, such syntax of test does not exist."
+				exit 8
+			fi
+		else
+			echo "Error in line $i of config file, such syntax of test does not exist."
+			#todo add removing tmp files
+			exit 9
 		fi
 	fi
+done
 
 	
-	
-done
