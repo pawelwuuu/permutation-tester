7ed0355f3c34823de5e7f4024edb61b53df57ffc
Pawel Wojcik <wojcikp376@gmail.com>
2023-01-19 15:54:15 -0500

    added pnginator

---
+++ pnginator.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+title=$(tail -n 1 imput.txt)
+output_file=$title.png
+
+head -n -1 input.txt | sort -n > sorted_file.txt
+
+gnuplot <<- EOF
+set datafile separator ' '
+set title "permutacje"
+set xlabel "permutacje"
+set ylabel "czas"
+set key top right
+set grid
+set term png
+set output "$output_file"
+plot 'sorted_file.txt' using 1:2 with lines
+EOF
